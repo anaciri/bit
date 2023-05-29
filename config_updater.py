0348f0362c793b93e503d39a356d35340114c428
Ayoub Naciri <ayb@ayoubs-mbp-2.mynetworksettings.com>
2023-05-29 09:40:08 -0400

    genesis

--- config_updater.py
+++ config_updater.py
@@ -12,7 +12,7 @@ def update_config(csv_file, json_file):
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    # Read the input data from the CSV file and update the JSON data
+    # update collateral from twreport.csv
     with open(input_file, 'r') as f:
         reader = csv.reader(f)
         next(reader)  # Skip the first row (header)
@@ -20,6 +20,16 @@ def update_config(csv_file, json_file):
             key, _, new_value, _ = row
             new_value = float(new_value.strip())  # Strip leading/trailing whitespace characters
             data['MARKET_MAP'][key]['START_COLLATERAL'] = new_value
+
+    # update margin rates from margin.csv
+    with open(input_file, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the first row (header)
+        for row in reader:
+            key, _, new_value, _ = row
+            new_value = float(new_value.strip())  # Strip leading/trailing whitespace characters
+            data['MARKET_MAP'][key]['START_COLLATERAL'] = new_value
+
 
     # Write the updated JSON data to the output file
     with open(output_file, 'w') as f:
