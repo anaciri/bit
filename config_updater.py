e33394e6eaa5d815134ca81d51eaf20572195b80
Ayoub Naciri <ayb@Ayoubs-MacBook-Pro-2.local>
2023-05-31 11:22:41 -0400

    adinstancearg

--- config_updater.py
+++ config_updater.py
@@ -2,15 +2,15 @@ import csv
 import json
 import argparse
 
-def update_config(csv_file, json_file):
+def update_config(csv_file, json_file, instance):
     input_file = csv_file
-    output_file = 'draft.config'
+    output_file = f'{instance}draft.config'
 
     # Read the JSON data from the config file
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    # Update collateral and basis margin from twreport.csv. use endCollat col as new START_COLLAT
+    # Update collateral and basis margin from twreport.csv
     with open(input_file, 'r') as f:
         reader = csv.reader(f)
         next(reader)  # Skip the first row (header)
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('csv_file', help='CSV file containing data to update the config file.')
     parser.add_argument('json_file', help='JSON config file to update.')
+    parser.add_argument('instance', help='Instance value to prepend to the output file name.')
     args = parser.parse_args()
 
-    update_config(args.csv_file, args.json_file)
+    update_config(args.csv_file, args.json_file, args.instance)
