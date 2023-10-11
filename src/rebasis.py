9797fd4506aeb550c3a57a66f161eb3c92a5d0bd
Ayoub Naciri <ayb@ayoubs-mbp-2.mynetworksettings.com>
2023-10-11 11:32:22 -0400

    fcb

--- src/rebasis.py
+++ src/rebasis.py
@@ -20,11 +20,12 @@ def update_config(csv_file, json_file, instance):
         reader = csv.reader(f)
         next(reader)  # Skip the first row (header)
         for row in reader:
-            key, _, collateral, margin, _ = row
+            key, collateral, _, fcb, _ = row
+            #key, _, collateral, margin, _ = row
             collateral = float(collateral.strip())
             margin = float(margin.strip())
             data['MARKET_MAP'][key]['START_COLLATERAL'] = collateral
-            data['MARKET_MAP'][key]['RESET_MARGIN'] = margin
+            data['MARKET_MAP'][key]['FC_BASIS'] = fcb
 
     # Write the updated JSON data to the output file
     with open(output_file, 'w') as f:
